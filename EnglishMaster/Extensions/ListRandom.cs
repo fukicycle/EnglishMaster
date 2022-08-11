db6ba76fadf78dd4b48ea12fb3b937e3d4485944
sato <sato@itsoft.local>
2022-08-12 08:48:08 +0900

    単語に複数の意味があった場合、選択肢に両方表示され 片方しか正解にならないバグを修正

--- EnglishMaster/Extensions/ListRandom.cs
+++ EnglishMaster/Extensions/ListRandom.cs
@@ -2,14 +2,14 @@
 {
     public static class ListRandom
     {
-        public static List<T> CreateRondom<T>(this List<T> items)
+        public static IEnumerable<T> CreateRandom<T>(this IEnumerable<T> items)
         {
             return items.Select(a => new { a, o = Guid.NewGuid() }).OrderBy(a => a.o).Select(a => a.a).ToList();
         }
 
-        public static T GetRandomFirstOrDefault<T>(this List<T> items)
+        public static T GetRandomFirstOrDefault<T>(this IEnumerable<T> items)
         {
-            return items.CreateRondom().FirstOrDefault();
+            return items.CreateRandom().FirstOrDefault();
         }
     }
 }

3619344fcf3024a41daef5b74d9f11d33d588562
sato <sato@itsoft.local>
2022-08-12 08:31:51 +0900

    add extension

---
+++ EnglishMaster/Extensions/ListRandom.cs
@@ -0,0 +1,15 @@
+﻿namespace EnglishMaster.Extensions
+{
+    public static class ListRandom
+    {
+        public static List<T> CreateRondom<T>(this List<T> items)
+        {
+            return items.Select(a => new { a, o = Guid.NewGuid() }).OrderBy(a => a.o).Select(a => a.a).ToList();
+        }
+
+        public static T GetRandomFirstOrDefault<T>(this List<T> items)
+        {
+            return items.CreateRondom().FirstOrDefault();
+        }
+    }
+}

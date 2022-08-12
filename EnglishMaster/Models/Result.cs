18cec4aa08c16aee1c916d4a4095bbe6dce4a8d1
sato <sato@itsoft.local>
2022-08-12 14:46:13 +0900

    答えられなかったページがあった際の表示を変更

--- EnglishMaster/Models/Result.cs
+++ EnglishMaster/Models/Result.cs
@@ -4,6 +4,6 @@
     {
         public Word Word { get; set; }
         public string Answer { get; set; }
-        public bool IsCorrect { get; set; }
+        public bool? IsCorrect { get; set; }
     }
 }

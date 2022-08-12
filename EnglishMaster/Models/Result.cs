5d891385806ce8b79e332dbbb4d7ae7cabb1e79b
sato <sato@itsoft.local>
2022-08-12 14:34:50 +0900

    add result entity

---
+++ EnglishMaster/Models/Result.cs
@@ -0,0 +1,9 @@
+﻿namespace EnglishMaster.Models
+{
+    public class Result
+    {
+        public Word Word { get; set; }
+        public string Answer { get; set; }
+        public bool IsCorrect { get; set; }
+    }
+}
